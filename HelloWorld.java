67bf984b0af783d3ba13f397b158d63508c977cb
SmrutiBastiaGit <68185757+SmrutiBastiaGit@users.noreply.github.com>
2020-07-12 20:51:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{  
         public static void main(String args[]){  
-         System.out.println("Hello Java new");  
+         System.out.println("Hello Java new poll scm");  
         }  
     }
